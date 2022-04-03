722663f93a3375f59e7c8ad65019efd0f81669bb
Şahin <sahincakmak7@gmail.com>
2022-04-03 19:35:03 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String zodiac = null;
+        int birthday;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lüften doğum tarihinizi giriniz :");
+        birthday = input.nextInt();
+
+        if (birthday % 12 == 0) {
+            zodiac = "Maymun";
+        }
+        if (birthday % 12 == 1) {
+            zodiac = "Horoz";
+        }
+        if (birthday % 12 == 2) {
+            zodiac = "Köpek";
+        }
+        if (birthday % 12 == 3) {
+            zodiac = "Domuz";
+        }
+        if (birthday % 12 == 4) {
+            zodiac = "Fare";
+        }
+        if (birthday % 12 == 5) {
+            zodiac = "Öküz";
+        }
+        if (birthday % 12 == 6) {
+            zodiac = "Kaplan";
+        }
+        if (birthday % 12 == 7) {
+            zodiac = "Tavşan";
+        }
+        if (birthday % 12 == 8) {
+            zodiac = "Ejderha";
+        }
+        if (birthday % 12 == 9) {
+            zodiac = "Yılan";
+        }
+        if (birthday % 12 == 10) {
+            zodiac = "At";
+        }
+        if (birthday % 12 == 11) {
+            zodiac = "Koyun";
+        }
+        System.out.println("Zodyağınız :"+zodiac);
+
+    }
+}
